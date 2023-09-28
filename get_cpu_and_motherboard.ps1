4ad07cff521af111e8b204fdffce2b8ba312d78d
Luis <31740475+luivelmor@users.noreply.github.com>
2023-09-28 13:07:45 +0200

    backup

---
+++ get_cpu_and_motherboard.ps1
@@ -0,0 +1,14 @@
+﻿###############################################
+# obtenemos la información cpu & motherboard
+###############################################
+$Win32_Baseboard = Get-CimInstance -ClassName Win32_Baseboard
+$Win32_Processor = Get-CimInstance -ClassName Win32_Processor
+
+# cpu
+$cpu = $Win32_Processor.Name | Out-String
+$cpu = $cpu.Trim()
+# motherboard
+$motherboard = ($Win32_Baseboard.Manufacturer + " - " + $Win32_Baseboard.Product) | Out-String
+$motherboard = $motherboard.Trim()
+
+Write-Host $cpu "-" $motherboard
